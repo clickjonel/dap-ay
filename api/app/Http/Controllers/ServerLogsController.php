f4a48513f619105c47516a6190b103a852f1da44
ramlittle <ramelitomartinez@gmail.com>
2025-11-13 12:03:05 +0800

    bugfix: solved for search function in server logs

--- api/app/Http/Controllers/ServerLogsController.php
+++ api/app/Http/Controllers/ServerLogsController.php
@@ -3,23 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\ServerLog;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ServerLogsController extends Controller
 {
-    public function readAllServerLogs()
+    public function readAllServerLogs(Request $request)
     {
-        $records = ServerLog::query()->with('user')->simplePaginate(10);
+        $records = ServerLog::query()
+            ->when(isset($request->keyword), function ($query) use ($request) {
+                $query->where('action_done', 'LIKE', "%{$request->keyword}%");
+            })
+            ->with('user')
+            ->orderBy('id', 'DESC')
+            ->simplePaginate(10);
         return response()->json($records, 200);
     }
 
+
+
     public function createServerLog(Request $request)
     {
-        $validatedData=$request->validate([
+        $validatedData = $request->validate([
             'created_by_id' => 'integer',
             'action_done' => 'string|max:255',
             'table_name' => 'string|max:255',
-            'column_id'=>'nullable|string|max:255'
+            'column_id' => 'nullable|string|max:255'
         ]);
         $record = ServerLog::create($validatedData);
         return response()->json([
